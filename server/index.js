9c757b1b6ee8fb1f0679ceffd81ec5abf0eaf842
parthmodi152 <modi.parth152@gmail.com>
2021-01-16 01:45:47 +0530

    v1 - without auth

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+import morgan from "morgan";
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/posts", postRoutes);
+
+// Listen and Connect to DB
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
